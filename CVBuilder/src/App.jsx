45d650020eec6c14bdbf80c8d01e7954f58cdfc4
Luciano Pulido <lucianonicolaspulido@gmail.com>
2024-03-15 10:52:50 -0300

    Separate main in components

--- CVBuilder/src/App.jsx
+++ CVBuilder/src/App.jsx
@@ -1,5 +1,7 @@
 import { useState } from "react"
 import Header from "./components/header"
+import Aside from "./components/aside";
+import Viewer from "./components/viewer";
 
 function App() {
   
@@ -9,15 +11,8 @@ function App() {
     <>
       <Header setLanguage={setLanguage}></Header>
       <main className="tablet:flex min-h-full">
-        <aside className="overflow-auto tablet:w-1/3 dark:text-white">
-          <button>Designs</button>
-          <button>Information</button>
-        </aside>
-        <section className="bg-gray-800 grid grid-c tablet:w-2/3 overflow-auto">
-          <article className="bg-white aspect-a4 mx-auto my-8 w-2/3">
-            
-          </article>
-        </section>
+        <Aside></Aside>
+        <Viewer></Viewer>
       </main>
     </>
   )
