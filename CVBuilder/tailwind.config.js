4affb35afae336b9b42f7caf5fa049178386d7cb
Luciano Pulido <lucianonicolaspulido@gmail.com>
2024-03-15 09:56:00 -0300

    Add aspect ratio a4 and custom screens

--- CVBuilder/tailwind.config.js
+++ CVBuilder/tailwind.config.js
@@ -3,7 +3,18 @@ module.exports = {
   content: ["./src/**/*.{html,js,jsx}"],
   darkMode: 'selector',
   theme: {
-    extend: {},
+    extend: {
+      aspectRatio:{
+        "a4": "210 / 297"
+      },
+    },
+    screens:{
+      "mobile-s": "320px",
+      "mobile-m": "375px",
+      "mobile-l": "425px",
+      "tablet": "768px",
+      "laptop": "1024px"
+    }
   },
   plugins: [],
 }
