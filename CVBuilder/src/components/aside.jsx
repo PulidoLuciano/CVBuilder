9004ff11176abc9901ef0f64b796452107588ae3
Luciano Pulido <lucianonicolaspulido@gmail.com>
2024-03-15 10:53:41 -0300

    Create aside component and customize nav

---
+++ CVBuilder/src/components/aside.jsx
@@ -0,0 +1,28 @@
+import { useState } from "react"
+
+export default function Aside(){
+    
+    const [sectionShown, setSectionShown] = useState("designs");
+    
+    return(
+        <aside className="overflow-auto tablet:w-1/3 dark:text-white">
+            <nav className="p-3">
+                <button onClick={() => setSectionShown("designs")} className={`navButton mr-3 ${(sectionShown == "designs") ? "bg-slate-500" : "bg-none"}`}>Designs</button>
+                <button onClick={() => setSectionShown("information")} className={`navButton ${(sectionShown != "designs") ? "bg-slate-500" : "bg-none"}`}>Information</button>
+            </nav>
+            <hr className="w-11/12 mx-auto"/>
+            {
+                (sectionShown == "designs")
+                ?
+                <section>
+                    <h2>Designs</h2>
+                </section>
+                :
+                <section>
+                    <h2>Information</h2>
+                </section>
+            }
+            
+        </aside>
+    )
+}
