cb8fb8a0daa49275a275f15d3ac02d195bbe6293
Luciano Pulido <lucianonicolaspulido@gmail.com>
2024-03-15 10:53:20 -0300

    Create viewer component

---
+++ CVBuilder/src/components/viewer.jsx
@@ -0,0 +1,9 @@
+export default function Viewer(){
+    return(
+        <section className="bg-gray-400 dark:bg-gray-800 grid grid-c tablet:w-2/3 overflow-auto">
+          <article className="bg-white aspect-a4 mx-auto my-8 w-2/3">
+            
+          </article>
+        </section>
+    )
+}
