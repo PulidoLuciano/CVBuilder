815b18cc1cf4a435e5ba26392215854a87a9fcd4
Luciano Pulido <lucianonicolaspulido@gmail.com>
2024-03-15 09:56:58 -0300

    Display general layout

--- CVBuilder/src/App.jsx
+++ CVBuilder/src/App.jsx
@@ -1,9 +1,24 @@
+import { useState } from "react"
 import Header from "./components/header"
 
 function App() {
+  
+  const [language, setLanguage] = useState(null);
+  
   return(
     <>
-      <Header></Header>
+      <Header setLanguage={setLanguage}></Header>
+      <main className="tablet:flex min-h-full">
+        <aside className="tablet:w-1/3">
+          <button>Designs</button>
+          <button>Information</button>
+        </aside>
+        <section className="bg-gray-800 grid grid-c tablet:w-2/3">
+          <article className="bg-white aspect-a4 mx-auto my-8 w-2/3">
+            
+          </article>
+        </section>
+      </main>
     </>
   )
 }
