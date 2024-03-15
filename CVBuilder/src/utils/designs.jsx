965f7d3c7c11a8e7449d410850e91825ee86b004
Luciano Pulido <lucianonicolaspulido@gmail.com>
2024-03-15 12:35:47 -0300

    Create designs.jsx and establish preview for 1,2 and 3

---
+++ CVBuilder/src/utils/designs.jsx
@@ -0,0 +1,44 @@
+export default
+[
+    {
+        id: 1,
+        preview: <div className="design-preview bg-white"></div>,
+    },
+    {
+        id: 2,
+        preview: 
+            <div className="design-preview bg-white">
+                <div className="h-full bg-default-primary w-2/6"></div>
+            </div>,
+    },
+    {
+        id: 3,
+        preview: 
+            <div className="design-preview bg-white">
+                <div className="bg-default-primary h-1/5"></div>
+                <div className="flex h-[80%]">
+                    <div className="bg-default-secondary w-2/6"></div>
+                </div>
+            </div>,
+    },
+    {
+        id: 4,
+        preview: <div className="design-preview bg-white"></div>,
+    },
+    {
+        id: 5,
+        preview: <div className="design-preview bg-white"></div>,
+    },
+    {
+        id: 6,
+        preview: <div className="design-preview bg-white"></div>,
+    },
+    {
+        id: 7,
+        preview: <div className="design-preview bg-white"></div>,
+    },
+    {
+        id: 8,
+        preview: <div className="design-preview bg-white"></div>,
+    }
+]
