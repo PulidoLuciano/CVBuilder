74b265178f844664703ae1ac8e3e5e1654c1aa0b
Luciano Pulido <lucianonicolaspulido@gmail.com>
2024-03-18 15:39:39 -0300

    Add style for selected design

--- CVBuilder/src/components/aside.jsx
+++ CVBuilder/src/components/aside.jsx
@@ -1,7 +1,7 @@
 import { useState } from "react"
 import designs from "../utils/designs";
 
-export default function Aside({text, setDesign}){
+export default function Aside({text, selectedDesign, setDesign}){
     
     const [sectionShown, setSectionShown] = useState("designs");
 
@@ -28,7 +28,7 @@ export default function Aside({text, setDesign}){
                     <div className="grid grid-cols-3 gap-3">
                         {
                             designs.map((design) => 
-                                <button key={design.id} id={design.id} onClick={changeDesign} className="flex items-center justify-center bg-gray-400 dark:bg-gray-800 p-3 w-full">
+                                <button key={design.id} id={design.id} onClick={changeDesign} className={(selectedDesign.id != design.id) ? "flex items-center justify-center bg-gray-400 dark:bg-gray-800 p-3 w-full" : "flex items-center justify-center bg-gray-900 dark:bg-gray-300 p-3 w-full"}>
                                     {design.preview}
                                 </button>
                             )
